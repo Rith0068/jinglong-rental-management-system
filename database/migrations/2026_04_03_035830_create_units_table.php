a9e4662be3aeaad00d30632deccdd60e7c8b1f03
Rith0068 <sothearithkoem@gmail.com>
2026-04-03 14:04:27 +0700

    feate migration

---
+++ database/migrations/2026_04_03_035830_create_units_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer("phone_number");
+            $table->string("rent_monthly");
+            $table->string("status");
+            $table->string("floor");
+            $table->unsignedBigInteger('property_id');
+            $table->foreign('property_id')->references('id')->on('properties');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('units');
+    }
+};
