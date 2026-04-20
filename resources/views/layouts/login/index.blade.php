4af61bb3c89d508a1625f5953dbfca7bf193e323
Rith0068 <sothearithkoem@gmail.com>
2026-04-20 10:40:26 +0700

    feat login page

---
+++ resources/views/layouts/login/index.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4">
+    <script type="module" src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <div class="flex">
+        <div class="block m-auto w-[440px] border-black shadow-2xl rounded-lg mt-[100px] h-[510px] bg-white-100">
+            <div class="flex">
+                <h2 class="pl-[50px] text-[32px] font-bold pt-[30px]">
+                    Welcome To JingLong
+                </h2>
+            </div>
+            <div class="flex font-bold text[16px] text-gray-400">
+                <p class="pl-[50px]">
+                    Regiser you account
+                </p>
+            </div>
+            <form action="" class="pl-[50px]">
+                <label>
+                    <p class="font-bold font-bold text[16px] mt-[20px]">
+                        Name
+                    </p>
+                    <input type="text" placeholder="fill your name nere" class="border-gray-800 text-gray-500 p-[10px] w-[330px] border-gray-950 font-bold bg-gray-50 rounded-lg hover:text-gray-800 mt-1">
+                </label>
+                <label>
+                    <p  class="font-bold font-bold text[16px] mt-[20px]">
+                        Gmail
+                    </p>
+                    <input type="gmail" placeholder="fill your gmail here" class="border-gray-800 text-gray-500 p-[10px] w-[330px] bg-gray-50 font-bold rounded-lg hover:text-gray-800 mt-1">
+                </label>
+                <label>
+                    <p class="font-bold font-bold text[16px] mt-[20px]">
+                        PassWord
+                    </p>
+                    <input type="" id="password" placeholder="fill your password here" class="border-gray-800 text-gray-500 p-[10px] w-[330px] bg-gray-50 font-bold rounded-lg hover:text-gray-800 mt-1">
+                </label>
+                <div class="flex font-bold text-[14px] text-gray-400 mt-[10px] gap-[10px]">
+                    <p class="">hide password</p>
+                    <input type="checkbox" id="toggleCheckbox" onclick="showPassWord()" class="mr-[10px]">
+                </div>
+            </form>
+            <div class="pl-[50px] flex justify-between pr-[50px]">
+                <button class="mt-[20px] bg-blue-400 p-[10px] w-[150px] rounded-lg shadow font-bold text[16px] text-gray hover:text-gray-50 hover:shadow-blue-300 hover:p-[11px] hover:w-[150px]">
+                    Login
+                </button>
+                
+                <script>
+                    function showPassWord() {
+                    var x = document.getElementById("password");
+                    if (x.type === "password") {
+                        x.type = "text";
+                    } else {
+                        x.type = "password";
+                    }
+                    }
+                </script>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RentController;
+
+Route::get('/login',[LoginController::class , 'login']);
+
+Route::get('/rent',[RentController::class , 'index']);
 
 Route::get('/', function () {
     return view('welcome');
