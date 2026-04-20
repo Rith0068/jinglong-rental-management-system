39f329162279fe02a9b4e615b635c21fc4a7a235
Rith0068 <sothearithkoem@gmail.com>
2026-04-20 15:43:50 +0700

    feat side bar

---
+++ resources/views/components/side-bar.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type="module" src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+</head>
+<body>
+    <div class="flex flex-col gap-6 bg-light border-r border-line h-full px-5 py-5 fixed bottom-0 left-0 border-t w-[250px] z-1000">
+        <div class="flex gap-[10px]">
+            <i class="fa-solid fa-bars mt-[15px] text-[20px] font-bold" id="sideBar" onclick="mySideBar()"></i>
+            <h2 class="font-bold text-[32px]">
+                Jing Long
+            </h2>
+        </div>
+        <hr>
+        <div class="block ">
+            <div class="flex gap-5">
+            <i class="fa-solid fa-table-columns mt-[15.5px]"></i>
+                <a href="" class="pt-[10px] font-bold text-[18px]"> Dashboard </a>
+            </div>
+            <div class="flex gap-5 ">
+                <i class="fa-regular fa-house  mt-[15.5px]"></i>
+                <a href="" class="pt-[10px] font-bold text-[18px]"> Rooms & Buildings </a>
+            </div>
+            <div class="flex gap-5">
+                <i class="fa-brands fa-cash-app  mt-[15.5px]"></i>
+                <a href="" class="pt-[10px] font-bold text-[18px]"> Rent Payments </a>
+            </div>
+            <div class="flex gap-5">
+                <i class="fa-solid fa-file-contract mt-[15.5px]"></i>
+                <a href="" class="pt-[10px] font-bold text-[18px]"> Contracts </a>
+            </div>
+            <div class="flex gap-5">
+                <i class="fa-solid fa-screwdriver-wrench mt-[15.5px]"></i>
+                <a href="" class="pt-[10px] font-bold text-[18px]">Maintenance      </a>
+            </div>
+        </div>
+        <hr>
+        <div>
+            
+        </div>
+  </div>
+</body>
+</html>
